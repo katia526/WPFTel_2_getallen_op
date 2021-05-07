f30bf319947951bd867bb5c9821eecfd04e1a750
katia verachtert <katia.verachtert.scherpendries18@outlook.com>
2021-05-07 16:00:36 +0200

    Add project files.

---
+++ WPFoefening3/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFoefening3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            var totaal = Convert.ToInt32(txt1.Text) + Convert.ToInt32(txt2.Text);
+            MessageBox.Show($"de som van {txt1} en {txt2} is {totaal}");
+        }
+    }
+}
